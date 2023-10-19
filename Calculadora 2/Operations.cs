8518bc26c0ff5e03617f73cfabea588af5a8a4d7
Matheus Sousa <matheuscarneirosousa1@gmail.com>
2023-10-18 22:14:58 -0300

    Atualização da calculadora

---
+++ Calculadora 2/Operations.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Calculadora_2
+{
+    internal class Operations
+    {
+        public void Validation(double k)
+        {
+            while (true)
+            {
+                try
+                {
+                    k = double.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("A entrada não é valida");
+                    break;
+                }
+            }
+        }
+        public void SumValues(double a, double b)
+        {
+            double Sum = a + b;
+            Console.WriteLine("A soma dos valores é " + Sum);
+        }
+        public void SubValues(double a, double b)
+        {
+            double Sub = a - b;
+            Console.WriteLine("A subtração dos valores é "+ Sub);
+        }
+        public void MulValues(double a, double b)
+        {
+            double Result = 0;
+            for (double i = 0; i < b; i++)
+            {
+                Result += a;
+            }
+            Console.WriteLine("A multiplicação dos valores é " + Result);
+        }
+        public void DivValues(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("A divisão por zero é impossível");
+            }
+            else
+            {
+                double Result = 0;
+                while (a >= b)
+                {
+                    a -= b;
+                    Result++;
+                }
+                Console.WriteLine("A divisão inteira dos valores é " + Result);
+                // Não consegui fazer a divisão fracionária, apenas a inteira
+            }
+        }
+    }
+}
---
+++ Calculadora 2/Program.cs
@@ -0,0 +1,86 @@
+﻿using Calculadora_2;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double FirstNumber;
+            double SecondNumber;
+            int Choice;
+            Operations op = new Operations();
+            while (true)
+            {
+
+                Console.Write("Digite o valor do primeiro número: ");
+                try
+                {
+                    FirstNumber = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("A entrada não é valida");
+                    break;
+                }
+                Console.Write("Digite o valor do segundo número: ");
+                try
+                {
+                    SecondNumber = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("A entrada não é valida");
+                    break;
+                }
+                Console.WriteLine("==== Operações ====");
+                Console.WriteLine("1 - Adição");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                Console.Write("Digite o código da operação desejada: ");
+                try
+                {
+                    Choice = int.Parse(Console.ReadLine());
+                                    while (Choice > 4 || Choice < 1)
+                {
+                    Console.WriteLine("==========================");
+                    Console.WriteLine("Digite uma operação válida");
+                    Console.Write("Digite o código da operação desejada: ");
+                    Choice = int.Parse(Console.ReadLine());
+                }
+                if (Choice == 1)
+                {
+                    op.SumValues(FirstNumber, SecondNumber);
+                }
+                if (Choice == 2)
+                {
+                    op.SubValues(FirstNumber, SecondNumber);
+                }
+                if (Choice == 3)
+                {
+                    op.MulValues(FirstNumber, SecondNumber);
+                }
+                if (Choice == 4)
+                {
+                    op.DivValues(FirstNumber, SecondNumber);
+                }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("A entrada não é válida");
+                }
+
+                Console.WriteLine("======================");
+                Console.WriteLine("Deseja fazer outra operação?");
+                Console.Write("Aperte Enter para Continuar ou Digite 'SAIR' para finalizar: ");
+                string Continue = Console.ReadLine().ToUpper();
+                if (Continue == "SAIR")
+                {
+                    Console.WriteLine("Volte sempre Joe ^^");
+                    break;
+                }
+            }
+        }
+    }
+}
